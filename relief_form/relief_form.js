c328dcd147444f93c0bf59f387c00184d336eb3f
sahil432 <gupta.sahil7959@gmail.com>
2017-08-07 20:21:23 +0530

    hbjh

---
+++ relief_form/relief_form.js
@@ -0,0 +1,22 @@
+
+// function searchJs(obj){
+	// var mobileFieldId = obj.id;
+	//alert(mobileFieldId);
+	// alert(jQuery("#mobile_no").val());
+	// var mobileNoLenght = jQuery("#mobile_no").val().length;
+	// if(mobileNoLenght == 10) {
+		// var mobileNo = jQuery('#'+mobileFieldId).val();
+		// alert(mobileNo);
+		// /*jQuery.ajax({
+			// type: 'POST',
+			// data: mobileNo,
+			// url: 'abc',
+			// success:function(data){
+				
+			// },
+			// error:function({
+				
+			// })
+		// }); */
+	// }
+// }
